f5983d26539a2a1921ba55393a37e55f7740aa8a
Shariq <shariqanwar20@hotmail.com>
2021-02-27 23:32:28 +0500

    deployed hello world website using aws

--- lib/hello_world-stack.ts
+++ lib/hello_world-stack.ts
@@ -1,20 +1,38 @@
 import * as cdk from "@aws-cdk/core";
-import * as lamda from "@aws-cdk/aws-lambda";
-import * as apigtw from "@aws-cdk/aws-apigateway";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
+import * as s3Deploy from "@aws-cdk/aws-s3-deployment";
 
 export class HelloWorldStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const hello = new lamda.Function(this, "HelloHandler", {
-      runtime: lamda.Runtime.NODEJS_10_X,
-      code: lamda.Code.fromAsset("lambda"),
-      handler: "hello.handler",
+    const websiteBucket = new s3.Bucket(this, "Website Bucket", {
+      versioned: true,
     });
 
-    new apigtw.LambdaRestApi(this, "Endpoint", {
-      handler: hello,
+    const distribution = new cloudfront.Distribution(
+      this,
+      "Distribution Domain Name",
+      {
+        defaultBehavior: {
+          origin: new origins.S3Origin(websiteBucket),
+        },
+        defaultRootObject: "index.html",
+      }
+    );
+
+    new cdk.CfnOutput(this, "DomainName", {
+      value: distribution.domainName,
+    });
+
+    new s3Deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3Deploy.Source.asset("./website")],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ["/*"],
     });
   }
 }
